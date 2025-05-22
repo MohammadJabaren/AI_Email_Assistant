6a38f35cebad95587790bbe64d822bc109163f77
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-05-22 11:54:05 +0300

    test

--- pages/write.tsx
+++ pages/write.tsx
@@ -3,7 +3,7 @@ import EmailActionPage from '../components/Email/EmailActionPage';
 const WriteEmailPage = () => {
   return (
     <EmailActionPage
-      title="Write New Email"
+      title="Write New Email ()"
       action="write"
       placeholder="Enter the topic or key points for your email..."
     />
