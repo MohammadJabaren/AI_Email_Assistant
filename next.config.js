fc8033f0c4978436275001179ae6b5a16fe85eba
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-05-23 14:42:56 +0300

    fix refresh

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  reactStrictMode: process.env.NODE_ENV === 'production',
+  reactStrictMode: true,
   
   // Configure to use Turbopack in development
   experimental: {
